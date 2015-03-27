529fb82b8b6896d6350bcc2ea06628e799689f2e
Skidmarkii <tu_k@naver.com>
2015-03-27 16:03:38 +0900

    opencv를 이용하여 비디오 읽어오기

--- src/analysisAlgorithm/AnalysisTopology.java
+++ src/analysisAlgorithm/AnalysisTopology.java
@@ -16,19 +16,19 @@ public class AnalysisTopology { // Topology that analysis image and recognize a
 
 		// attach a spout to emit images from video.
 		tBuilder.setSpout("image-Spout", new ImageSpout(), 1);
-
+/*
 		// attach a bolt to recognize a 'Thing' from video.
-		tBuilder.setBolt("recog-Bolt", new RecogBolt(), 10).shuffleGrouping(
+		tBuilder.setBolt("recog-Bolt", new RecogBolt(), 1).shuffleGrouping(
 				"image-Spout");
 
 		// attach a bolt to count a 'Thing' from stream.
-		tBuilder.setBolt("count-Bolt", new CountBolt(), 15).fieldsGrouping(
+		tBuilder.setBolt("count-Bolt", new CountBolt(), 1).fieldsGrouping(
 				"recog-Bolt", new Fields("iName"));
 
 		// attach a bolt to report a result. using globalGrouping
 		tBuilder.setBolt("report-Bolt", new ReportBolt(), 1).globalGrouping(
 				"count-Bolt");
-
+*/
 		Config conf = new Config();
 
 		conf.setDebug(true);
@@ -38,7 +38,7 @@ public class AnalysisTopology { // Topology that analysis image and recognize a
 			StormSubmitter.submitTopology(args[0], conf,
 					tBuilder.createTopology());
 		} else {											// on Local cluster
-			conf.setMaxTaskParallelism(3);
+			conf.setMaxTaskParallelism(1);
 			LocalCluster cluster = new LocalCluster();
 			cluster.submitTopology("analysis-image", conf,
 					tBuilder.createTopology());
--- src/analysisAlgorithm/ImageSpout.java
+++ src/analysisAlgorithm/ImageSpout.java
@@ -13,6 +13,10 @@
  * imageCollector에서 영상에서 이미지를 추출하는 과정을 수행
  * 추출한 이미지를 특정경로 폴더에 저장하고 해당 폴더에 있는 이미지들을 읽어와서 emit
  * 
+ * 추가 변경사항
+ * 이미지를 분배하는게 아니라 영상 자체를 분배
+ * 하나의 스트림이 하나의 영상을 처리
+ * 이미지를 분배하면 신뢰도 급하락
  * 
  */
 
@@ -21,31 +25,46 @@ package analysisAlgorithm;
 import java.util.Map;
 import java.util.Random;
 
+import org.opencv.core.Core;
+import org.opencv.highgui.VideoCapture;
+
 import backtype.storm.spout.SpoutOutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
 
 public class ImageSpout extends BaseRichSpout {
+	
+	boolean checkOpenVideo;
 	SpoutOutputCollector _collector;
 	Random _rand;
+	VideoCapture vc;
 	
 	@Override
 	public void open(Map arg0, TopologyContext arg1, SpoutOutputCollector arg2) {
 		_collector = arg2;
 		_rand = new Random();
+		checkOpenVideo = true;
+		
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		vc = new VideoCapture();
 	}
 
 	@Override
 	public void nextTuple() {
-		Utils.sleep(100);
+		Utils.sleep(1000);
+		// read video at the folder that stores video and emit video.
+		checkOpenVideo = vc.open("test_0.avi");
+		System.out.println(checkOpenVideo);
+		_collector.emit(new Values(vc));
 	}
-
 
 	@Override
 	public void declareOutputFields(OutputFieldsDeclarer arg0) {
 		// TODO Auto-generated method stub
-		
+		arg0.declare(new Fields("video_name"));
 	}
 }
